63cbd7ce0b2fca887467a47c1de38a3f8b8b2596
sakhamlbb <sakhamlbb@gmail.com>
2025-02-19 19:34:33 +0700

    update_kaella

---
+++ Kaela/PROJEK1/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",function () {
+    console.log("website sudah dimulai!");
+    
+    const button = document.querySelector(".btn");
+    button.addEventListener("click",function()
+}
+alert("anda akan di arahkan ke halaman berikutnya.");
+});
+});
